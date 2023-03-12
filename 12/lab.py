49692e277dbbd22b7940956d4f3d75df23112132
EmelyanovAlexey <43723067+EmelyanovAlexey@users.noreply.github.com>
2023-03-12 16:53:42 +0700

    12 таска

--- 12/lab.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-
-11 тема - Задание: "Написать SimpleModel другим способом. Использовать model = nn.Sequential() https://pytorch.org/tutorials/beginner/nn_tutorial.html?highlight=mnist ". + hw.pdf ( https://github.com/d-pack/LessonsPAK/blob/main/s14/hw.pdf )
--- 12/task.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-# 12 тема - 1) Задача классификации изображений (https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html?highlight=mnist). 
-# Повторить тренировку модели (train) и запустить классификацию изображений (inference). 2) Получить максимальную точность классификации (минимальный loss) путём изменения модели, например, 
-# добавлением скрытых слоёв. 3*) По возможности обучить на GPU.
